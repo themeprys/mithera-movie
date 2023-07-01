6e3857749a2de0db4a36903eb266162b78f55877
Priyo S <themeprys@gmail.com>
2023-07-02 06:17:45 +0700

    add new 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,8 @@ export default defineNuxtConfig({
         link: [
             { rel: 'icon', type: 'image/png', href: '/favicon.png' },
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700;900&display=swap'},
-            { rel: 'stylesheet', href: 'assets/css/main.css'},            
-            { rel: 'stylesheet', href: 'assets/css/responsive.css'},
+            { rel: 'stylesheet', href: '/assets/css/main.css'},            
+            { rel: 'stylesheet', href: '/assets/css/responsive.css'},
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;700&display=swap'}
         ],  
         script: [
