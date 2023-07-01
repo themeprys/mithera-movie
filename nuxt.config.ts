b93a4f9980f105dc011b82541694b62da99a4bbf
Priyo S <themeprys@gmail.com>
2023-07-02 06:54:13 +0700

    add robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,31 @@
 export default defineNuxtConfig({
   app: {
     head: {
-        link: [
-            { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700;900&display=swap'},
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;700&display=swap'}
-        ],  
-        script: [
-            { hid: 'fontawesome', src: 'https://kit.fontawesome.com/2c19e713ed.js', defer: true }
-          ]
-    },        
-},  
-  css: [
-    '~/assets/css/main.css',
-    '~/assets/css/responsive.css',    
-  ]
-
-})
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700;900&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Aleo:wght@300;400;700&display=swap",
+        },
+      ],
+      script: [
+        {
+          hid: "fontawesome",
+          src: "https://kit.fontawesome.com/2c19e713ed.js",
+          defer: true,
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.css", "~/assets/css/responsive.css"],
+  modules: [
+    '@nuxtjs/robots'
+  ],
+  robots: {
+    /* module options */
+  }
+});
