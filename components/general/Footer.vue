0e5bf0e52d0fcb9bbea5b77917276338dd8e0610
Priyo S <themeprys@gmail.com>
2023-07-02 23:52:07 +0700

    mobile

--- components/general/Footer.vue
+++ components/general/Footer.vue
@@ -1,83 +1,63 @@
 <template>
   <div class="mth_footer text-white">
-<div class="container">
-  <footer class="py-5">
-    <div class="row">
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Navigation</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#trailer" class="nav-link p-0 text-body-secondary">Trailer</a></li>
-          <li class="nav-item mb-2"><a href="#synopsys" class="nav-link p-0 text-body-secondary">Synopsis</a></li>
-          <!-- <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Cast & Crew</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Gallery</a></li> -->
-        </ul>
-      </div>
-
-      <div class="col-6 col-md-2 mb-3">
-        <h5>&nbsp;</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#cast" class="nav-link p-0 text-body-secondary">Cast & Crew</a></li>
-          <li class="nav-item mb-2"><a href="#gallery" class="nav-link p-0 text-body-secondary">Gallery</a></li>
-          <li class="nav-item mb-2"><a href="#statement" class="nav-link p-0 text-body-secondary">Director's Statement</a></li>
-        </ul>
-      </div>
-
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Powered by</h5>
-        <img src="~/public/img/mondiblanc.png" class="img-fluid" width="200px"/>        
-        <!-- <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-        </ul> -->
-      </div>
-
-      <div class="col-md-5 offset-md-1 mb-3">
-        <form>
-          <h5>Subscribe to Mithera newsletter</h5>
-          <p>Get updates from our latest feed</p>
-          <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-            <button class="btn btn-dark" type="button">Subscribe</button>
+    <div class="container">
+      <footer class="py-2 py-md-5">
+        <div class="row">
+          <div class="col-6 col-md-2 mb-3">
+            <h5 class="mobile_navi">Navigation</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+              <li class="nav-item mb-2"><a href="#trailer" class="nav-link p-0 text-body-secondary">Trailer</a></li>
+              <li class="nav-item mb-2"><a href="#synopsys" class="nav-link p-0 text-body-secondary">Synopsis</a></li>
+            </ul>
           </div>
-        </form>
-      </div>
-    </div>
+          <div class="col-6 col-md-3 mb-3">
+            <h5>&nbsp;</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#cast" class="nav-link p-0 text-body-secondary">Cast & Crew</a></li>
+              <li class="nav-item mb-2"><a href="#gallery" class="nav-link p-0 text-body-secondary">Gallery</a></li>
+              <li class="nav-item mb-2"><a href="#statement" class="nav-link p-0 text-body-secondary">Director's
+                  Statement</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md-3 mb-3 mt-5 mt-md-0">
+            <h5>Powered by</h5>
+            <img src="~/public/img/mondiblanc.png" class="img-fluid" width="200px" />
+          </div>
+          <div class="col-6 col-md-3 mb-3  mt-5 mt-md-0">
+            <form>
+              <h5>Follow Us</h5>
+              <p>Get updates from our latest feed</p>
+              <div class="d-flex flex-column flex-sm-row w-100 gap-2">
 
-    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-      <p>© 2023 Mondiblanc Film Workshop. All rights reserved.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-body-emphasis" target="_blank" href="https://www.instagram.com/mitherafilm/"><i class="fa-brands fa-instagram"></i></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="mailto:info@mondiblanc.org"><i class="fa-solid fa-envelope"></i></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="tel:+6285624011441"><i class="fa-solid fa-phone"></i></a></li>
-      </ul>
-    </div>
-  
-  </footer>
-</div>
-</div>
-  <!-- <div class="bg-dark text-white">
-    <div class="container text-center">
-      <div class="row">
-        <div class="col py-4">
-          <p>
-            Copyright by Mondiblanc @ 2023 - All Rights Reseved
-            <br />Design by
-            themePrys
-          </p>
+                <ul class="list-unstyled d-flex">
+                  <li class="ms-3"><a class="link-body-emphasis" target="_blank"
+                      href="https://www.instagram.com/mitherafilm/"><i class="fa-brands fa-instagram fa-1x"></i></a></li>
+                  <li class="ms-3"><a class="link-body-emphasis" href="mailto:info@mondiblanc.org"><i
+                        class="fa-solid fa-envelope fa-1x"></i></a></li>
+                  <li class="ms-3"><a class="link-body-emphasis" href="tel:+6285624011441"><i
+                        class="fa-solid fa-phone fa-1x"></i></a></li>
+                </ul>
+
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top text-center">
+          <p>© 2023 Mondiblanc Film Workshop. All rights reserved.</p>
+          <div class="text-center">
+            Made with&nbsp;&nbsp;<i class="fa-solid fa-heart"></i>&nbsp;&nbsp;by <a href="https://themeprys.vercel.app">themePrys</a>
+          </div>
+        </div>
+
+      </footer>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
